a9e58b1784089ba36459c3c3d4038312ea411df7
ahmedzein <elze0010@algonquinlive.com>
2015-12-10 19:23:28 -0500

    cleaned scrolling effect

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $('.nav').localScroll();
 
 $('.nav-footer').localScroll();
+
+$('.logo').mouseover(function(){
+  $('.logo').attr("src","images/name.svg");
+});
+
+$('.logo').mouseout(function(){
+  $('.logo').attr("src","images/namebw.svg");
+});
