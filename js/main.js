3e3292fdfb629da56d1345f77dfcf7b80ba7a68e
ahmedzein <elze0010@algonquinlive.com>
2015-12-14 15:10:55 -0500

    added social icons switch
    the icons switch over the hover through js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
+//Scroll on home page
+
 $('.nav').localScroll();
 
 $('.nav-footer').localScroll();
+
+
+//Logo changes when on hover
 
 $('.logo').mouseover(function(){
   $('.logo').attr("src","images/name.svg");
@@ -9,3 +14,34 @@ $('.logo').mouseover(function(){
 $('.logo').mouseout(function(){
   $('.logo').attr("src","images/namebw.svg");
 });
+
+//linkedin flip
+
+$('.linkedin').mouseover(function(){
+  $('.linkedin').attr("src","images/linkedin-logo.svg");
+});
+
+$('.linkedin').mouseout(function(){
+  $('.linkedin').attr("src","images/linkedin-logo-flip.svg");
+});
+
+//behance flip
+
+$('.behance').mouseover(function(){
+  $('.behance').attr("src","images/behnace-logo.svg");
+});
+
+$('.behance').mouseout(function(){
+  $('.behance').attr("src","images/behnace-logo-flip.svg");
+});
+
+//mail flip
+
+$('.mail').mouseover(function(){
+  $('.mail').attr("src","images/mail-icon.svg");
+});
+
+$('.mail').mouseout(function(){
+  $('.mail').attr("src","images/mail-icon-flip.svg");
+});
+
